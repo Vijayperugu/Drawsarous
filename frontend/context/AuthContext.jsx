a94c2343c8b90287a64124a9851d593292835819
Vijayperugu <124055443+Vijayperugu@users.noreply.github.com>
2025-07-14 22:14:13 +0530

    Update AuthContext.jsx

--- frontend/context/AuthContext.jsx
+++ frontend/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { io } from 'socket.io-client';
-const Backend_URL = import.meta.env.VITE_BACKEND_URLL;
+const Backend_URL = "https://drawsarous.onrender.com";
 
 export const AuthContext = createContext();
 
